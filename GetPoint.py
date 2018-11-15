265d5c12428465b4014313d7b22a75188fabfa05
hosisimezi <jheiuahurahuh.jahuaylaiv@gmail.com>
2018-11-15 13:31:53 +0900

    座標の自動保存を追加

--- GetPoint.py
+++ GetPoint.py
@@ -8,26 +8,112 @@ import pylab
 from PIL import Image
 import matplotlib.pyplot as plt
 import glob
-import sys
+import sys,csv,os
 
 args=sys.argv
 
+FLAG=False
+
 if(len(args) != 2):
-    print "ディレクトリを指定してください。"
-    print "例:~/Background/"
+    print( "ディレクトリを指定してください。")
+    print ("例:~/Background/")
     sys.exit()
 
+with open('./Getpoint.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['filename','xmin','ymin','xmax','ymax'])   
+
 def onclick(event):
-    print 'event.button=%d,  event.x=%d, event.y=%d, event.xdata=%f, \
-    event.ydata=%f'%(event.button, event.x, event.y, event.xdata, event.ydata)
+    global x1,y1,FLAG
+    print("click")
+    try:
+        FLAG=True
+        x1=int(round(event.xdata))
+        y1=int(round(event.ydata))
+    except:
+        return
+    
+
+def ondrag(event):
+    global x1,x2,y1,y2,x,y,FLAG
+    if FLAG==False:
+        return
+    try:
+        x2=int(round(event.xdata))
+        y2=int(round(event.ydata))
+        drawRect()
+        plt.draw()
+    except:
+        return
+
+def onkey(event):
+    global x1,y1,x2,y2
+    
+    if event.key=='e':
+        print("データを保存します")
+        x1, x2 = sorted([x1,x2])
+        y1, y2 = sorted([y1,y2])
+        data.append([base,x1,y1,x2,y2])
+        #print(data)
+
+    if event.key=='q':
+        print('現在の画像を終了します')
+        if data :
+            with open('./Getpoint.csv', 'a') as f:
+                writer = csv.writer(f)
+                for i in data:
+                    writer.writerow(i)   
+        plt.close()
+
+
+def onrelease(event):
+    global FLAG
+    FLAG=False
+
+
+def drawRect():
+    global x1,y1,x2,y2
+    Rect = [ [ [x1,x2], [y1,y1] ],
+             [ [x2,x2], [y1,y2] ],
+             [ [x1,x2], [y2,y2] ],
+             [ [x1,x1], [y1,y2] ] ]
+    for i, rect in enumerate(Rect):  
+        lns[i].set_data(rect[0],rect[1])
 
 
 for images in glob.glob(args[1]+"*"):
-    image=Image.open(images)
+    x1=0
+    y1=0
+    x2=0
+    y2=0
+    data=[]
+    base=os.path.basename(images)  
+    image=Image.open(images)  
     image_list=np.asarray(image)
 
+   
+
     plt.figure()
+    plt.subplot(1,1,1)
+
+    Rect = [ [ [x1,x2], [y1,y1] ],
+            [ [x2,x2], [y1,y2] ],
+            [ [x1,x2], [y2,y2] ],
+            [ [x1,x1], [y1,y2] ] ]
+
+    lns = []
+    for rect in Rect:
+        ln, = plt.plot(rect[0],rect[1],color='r',lw=2)
+        lns.append(ln)
+        
+    plt.connect('button_press_event', onclick)     
+    plt.connect('motion_notify_event',ondrag)
+    plt.connect('button_release_event',onrelease)
+    plt.connect('key_press_event',onkey)
+    
     plt.title(images)
-    plt.imshow(image_list)
-    plt.connect('button_press_event', onclick)     # マウスを押した時
+    im=plt.imshow(image_list)
+    plt.clim(im.get_clim())
+    plt.axis('off')
+
     plt.show()
