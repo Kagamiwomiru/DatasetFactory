92d07c5b0ccd6c6def13f5a55407a03cddfe72b5
Kagami <kagamiwomiru@me.com>
2018-12-24 16:26:06 +0900

    ラベルマップ完成。ラベルマップの指示通り、フォルダに1枚ずつ画像を入れていく

--- exe.sh
+++ exe.sh
@@ -1,4 +1,8 @@
-python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazou/ \
+
+
+for label in seq 1 48
+  do
+  python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazou/ \
                          --Output_dir=/home/kagamiwomiru/datasets/Load_set/ \
                          --Target_dir=/home/kagamiwomiru/datasets/hyousiki/ \
                          --annotation_file=/home/kagamiwomiru/datasets/Load_set_annotation.csv \
@@ -6,3 +10,5 @@ python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazo
                          --label=? \
                          --name=test \
                          --recipe_file=./Getpoint.csv
+  done
+
