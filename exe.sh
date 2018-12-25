0a8bc3b5d839178f883b3694ec7061c0bfc3091e
kagamiwomiru <kagamiwomiru@me.com>
2018-12-25 14:19:45 +0900

    exe.sh

--- exe.sh
+++ exe.sh
@@ -1,8 +1,11 @@
-python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazou/ \
+for label in `seq 45`
+  do
+  python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazou/ \
                          --Output_dir=/home/kagamiwomiru/datasets/Load_set/ \
                          --Target_dir=/home/kagamiwomiru/datasets/hyousiki/ \
                          --annotation_file=/home/kagamiwomiru/datasets/Load_set_annotation.csv \
                          --init=true \
-                         --label=? \
-                         --name=test \
+                         --label=$label \
+                         --name=$label \
                          --recipe_file=./Getpoint.csv
+  done
