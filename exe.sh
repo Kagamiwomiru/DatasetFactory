96aa94c433ab420f10bc307a21f8fa4134da5f5c
Kagamiwomiru <soromonneo@gmail.com>
2019-01-13 16:44:06 +0900

    変更内容をマージ

--- exe.sh
+++ exe.sh
@@ -1,19 +1,19 @@
 function last(){
   echo '強制終了'
-  rm -rf /home/kagamiwomiru/datasets/Load_set/imgs/
-  rm /home/kagamiwomiru/datasets/Load_set/Load_set_annotation.csv
+  rm -rf /home/kagamiwomiru/datasets/Load_set_sep/imgs/
+  rm /home/kagamiwomiru/datasets/Load_set_sep/Load_set_sep_annotation.csv
   exit 2
 
 }
 
 trap 'last' {1,2,3,15}
-echo "label,minX,minY,maxX,maxY,path,file,name" > /home/kagamiwomiru/datasets/Load_set/Load_set_annotation.csv
-for label in `seq 45`
+echo "label,minX,minY,maxX,maxY,path,file,name" > /home/kagamiwomiru/datasets/Load_set_sep/Load_set_sep_annotation.csv
+for label in `seq 44`
   do
   python DatasetFactory.py --Background_dir=/home/kagamiwomiru/datasets/haikeigazou/ \
-                         --Output_dir=/home/kagamiwomiru/datasets/Load_set/imgs/ \
+                         --Output_dir=/home/kagamiwomiru/datasets/Load_set_sep/imgs/ \
                          --Target_dir=/home/kagamiwomiru/datasets/hyousiki/$label/ \
-                         --annotation_file=/home/kagamiwomiru/datasets/Load_set/Load_set_annotation.csv \
+                         --annotation_file=/home/kagamiwomiru/datasets/Load_set_sep/Load_set_sep_annotation.csv \
                          --init=true \
                          --label=$label \
                          --name=$label \
